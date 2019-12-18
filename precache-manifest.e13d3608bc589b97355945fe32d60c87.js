363ed5d0d4dd6dafd6646f25156b377aff8a0db2
Zhabskyi <poltavagaz@gmail.com>
2019-12-17 17:53:02 -0700

    Updates

---
+++ precache-manifest.e13d3608bc589b97355945fe32d60c87.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e7174f885ce7d8c5367ca6992b5bbc5",
+    "url": "/zhabskyi/index.html"
+  },
+  {
+    "revision": "9a101bb6f74752f89e27",
+    "url": "/zhabskyi/static/css/main.02e33b17.chunk.css"
+  },
+  {
+    "revision": "4f7a66ce5babe79fc831",
+    "url": "/zhabskyi/static/js/2.625214aa.chunk.js"
+  },
+  {
+    "revision": "72312ca0e45d60ac383cae138630c0c7",
+    "url": "/zhabskyi/static/js/2.625214aa.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9a101bb6f74752f89e27",
+    "url": "/zhabskyi/static/js/main.959aa39a.chunk.js"
+  },
+  {
+    "revision": "a3e960fbdd1157545ab4",
+    "url": "/zhabskyi/static/js/runtime-main.ec7820d1.js"
+  },
+  {
+    "revision": "54d545ddcf93a9790da1d348b09c0beb",
+    "url": "/zhabskyi/static/media/arrow-upside.54d545dd.png"
+  },
+  {
+    "revision": "85ee83c9294b43dc46b7d1da5e839830",
+    "url": "/zhabskyi/static/media/arrow.85ee83c9.png"
+  },
+  {
+    "revision": "df263dba90004ad193163d15b14afe71",
+    "url": "/zhabskyi/static/media/footer_1.df263dba.jpg"
+  },
+  {
+    "revision": "b8160965ee5079f1f4d395d4d75542b3",
+    "url": "/zhabskyi/static/media/logo.b8160965.jpg"
+  },
+  {
+    "revision": "d851d2082931f5b8e9b93e0703a370c7",
+    "url": "/zhabskyi/static/media/portfolio_1.d851d208.jpg"
+  }
+]);
